a94739b0cb19484afe05b1785d6fad5941bf5732
“chengzhao” <yaolao@xiaohongshu.com>
2019-07-22 11:00:04 +0800

    libyuv source

---
+++ Application.mk
@@ -0,0 +1,7 @@
+APP_PLATFORM = android-8
+APP_ABI := all
+APP_STL := stlport_static
+APP_OPTIM := release
+
+# ndk-build NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=./Android.mk
+# APP_ABI := all  [arm64-v8a] [x86_64] [mips64] [armeabi-v7a] [armeabi] [x86] [mips]
---
+++ jni/Application.mk
@@ -0,0 +1,4 @@
+# ndk-build NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=./Android.mk
+APP_ABI := armeabi-v7a arm64-v8a
+APP_OPTIM := release
+
